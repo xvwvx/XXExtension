9ab9bf5949fbfa9042e3394e5e387b3485d45693
jangsky215 <339349330@qq.com>
2018-07-26 08:57:53 +0800

    修改实现

--- XXExtension/Core/Extension/UI/UIView+XXCollapsibleConstraints.swift
+++ XXExtension/Core/Extension/UI/UIView+XXCollapsibleConstraints.swift
@@ -132,12 +132,10 @@ extension UIView {
         
         if self.xx_autoCollapse && self.xx_collapsibleConstraints.count > 0 {
             let contentSize = self.intrinsicContentSize
-//            let noIntrinsicSize = CGSize(width: UIViewNoIntrinsicMetric,
-//                                         height: UIViewNoIntrinsicMetric)
-//
-//            let collapsed contentSize == noIntrinsicSize || contentSize == CGSize.zero
-            
-            let collapsed = contentSize.width <= 0 || contentSize.height <= 0
+            let noIntrinsicSize = CGSize(width: UIViewNoIntrinsicMetric,
+                                         height: UIViewNoIntrinsicMetric)
+
+            let collapsed = contentSize == noIntrinsicSize || contentSize == CGSize.zero
             self.xx_setCollapsed(collapsed, animationView: nil)
         }
     }
